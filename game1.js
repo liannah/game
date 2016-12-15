6b00d4f5bf0333da7d3a43b5264f250601eb20a8
liannah <liannahovhannisyan7@gmail.com>
2016-12-15 21:05:34 +0400

    first commit

--- game1.js
+++ game1.js
@@ -1,3 +1,5 @@
+
+
 let canvas = document.getElementById("canvas");
 let context = canvas.getContext("2d");
 
@@ -5,9 +7,11 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 
-const NUM_POINTS = 20;//20 + Math.random();
+const NUM_POINTS = 20;
 const points = [];
+const satkacikner = [];
 let ants = document.getElementsByClassName("points");
+
 
 for (let i = 0; i < NUM_POINTS; i++) {
 
@@ -26,6 +30,15 @@ for (let i = 0; i < NUM_POINTS; i++) {
 
     });
 
+    let item = {
+       
+        x: Math.random() * (canvas.width - 2 * size),
+
+    };
+    item.xDelta = item.width / 20;
+
+    points.push(item);
+
 }
 
 var leftimg = new Image();
@@ -34,17 +47,15 @@ leftimg.src = "./ant.png";
 var rightimg = new Image();
 rightimg.src= "./ant2.png";
 
+        
 var img = new Image();
 img.src= "./yellow.png"
 
 const draw = function () {
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    //context.clearRect(0, 0, canvas.width, canvas.height);
     points.forEach(function (l) {
         
-
-        if(l.isDead) { context.drawImage(img, l.x, l.y, l.width, l.height);
-
-        } else if(l.xDelta > 0) {
+        if(l.xDelta > 0) {
             context.drawImage(rightimg, l.x, l.y, l.width, l.height);
         } else  {
             context.drawImage(leftimg, l.x, l.y, l.width, l.height);
@@ -63,58 +74,47 @@ const draw = function () {
         }
 
     });
+
     drawScore();
-    }
-let animate = function () {
-    draw();
-    requestAnimationFrame(animate);
+
+}
+const drawsatkacikner = function(){
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    satkacikner.forEach(function (a) {
+            context.drawImage(img, a.x, a.y, a.width, a.height);
+    });
 }
 
-animate();
+var score = 0; 
 
 
 $("#canvas").on('mousedown', function(e){
 
-    const getMouseCo =function(){
-
-    points.forEach(function(p) {
-       var sound = new Audio('./split2.wav');
-
-        if (e.clientX > p.x && e.clientX < p.x + p.width && e.clientY > p.y && e.clientY < p.height+p.y){
-            p.isDead = true;
-            p.xDelta = 0;
-            p.yDelta = 0;
-            sound.play();
-}
-
-        }
-    )
-}
+    const getMouseCo = function(){
 
 
-    getMouseCo();
-});
-
-var score = 0;
-
-$("#canvas").on('mousedown', function(e){
-function detectionofclick() {
-
-points.forEach(function(p){
-        if (e.clientX > p.x && e.clientX < p.x + p.width && e.clientY > p.y && e.clientY < p.height+p.y) { 
-            score++; 
-         if(score > NUM_POINTS) {
+        points.forEach(function(p, idx){
+            var sound = new Audio('./split2.wav');
+            if (e.clientX > p.x && e.clientX < p.x + p.width && e.clientY > p.y && e.clientY < p.height+p.y){
+                p.isDead = true;
+                p.xDelta = 0;
+                p.yDelta = 0;
+                satkacikner.push(p);
+                points.splice(idx, 1);
+                sound.play();
+                score++;
+                if(score >= NUM_POINTS) {
                         alert("YOU WIN, CONGRATS!");
                         document.location.reload();
         }
-
+            }
+        
+        })
     }
-
-})
-}
-detectionofclick();
-
+    getMouseCo();
 });
+
 
 function drawScore() {
     context.font = "16px Arial";
@@ -122,11 +122,18 @@ function drawScore() {
     context.fillText("Score: "+score, 100, 20);
 }
 
+let animate = function () {
+    
+    drawsatkacikner();
 
+    draw();
+    requestAnimationFrame(animate);
+}
+
+animate();
 
 let sTime = new Date().getTime();
-let countDown =10;
-
+let countDown =30;
 
 function UpdateTime() {
     let cTime = new Date().getTime();
@@ -144,32 +151,23 @@ function UpdateTime() {
 }
 UpdateTime();
 let counter = setInterval(UpdateTime, 500);
-//where n is the number of minutes required.
- //function getTimeRemaining(endtime) {
-  //var t = Date.parse(endtime) - Date.parse(new Date());
-  //var seconds = Math.floor((t / 1000) % 60);
-  //return {
-    //'total': t,
-    //'seconds': seconds
-  //};
-//}
 
-//function initializeClock(id, endtime) {
-  //var clock = document.getElementById(id);
-  //var secondsSpan = clock.querySelector('.seconds');
-
-  //function updateClock() {
-    //var t = getTimeRemaining(endtime);
-
-    //secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-    //if (t.total <= 0) {
-      //alert("Game is over");
-    //}
-  //}
-
-  //updateClock();
-//}
-
-//var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
-//initializeClock('clockdiv', deadline);
+/*let over = false;
+function gameOver(over){
+    if(over){
+        render();
+        let gameoverImage = new Image();
+        gameoverImage.src = "gameover.png";
+        context.drawImage(gameOverImage, 350, 100);
+        
+    }
+}
+let timeover = false;
+function timeOver(timeover){
+    if(timeover){
+        render();
+        let timeupImage = new Image();
+        timeupImage.src = "timesup.png";
+        ctx.drawImage(timeupImage, 350, 100);
+    }
+}
